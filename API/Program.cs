9db70b1edb7b97478c3edcda568c574e9e7f36b7
Eoin O'Malley <eomalley@letsgetchecked.com>
2022-05-09 17:47:50 +0100

    Add Cors origin for client

--- API/Program.cs
+++ API/Program.cs
@@ -4,20 +4,24 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 IConfigurationRoot configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.Development.json")
-                .Build();
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.Development.json")
+    .Build();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(opt => 
-{
-    opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddDbContext<StoreContext>(
+    opt =>
+    {
+        opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+    }
+);
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -28,7 +32,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+app.UseCors(
+    opt =>
+    {
+        opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    }
+);
 
 app.UseAuthorization();
 
@@ -38,7 +49,7 @@ app.MapControllers();
 using var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-try 
+try
 {
     context.Database.Migrate();
     DbInitialiser.Initialise(context);
