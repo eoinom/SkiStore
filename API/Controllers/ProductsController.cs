2212796ada926af061a58d5381cb946803cd3d23
Eoin O'Malley <eomalley@letsgetchecked.com>
2022-05-09 15:57:25 +0100

    Fix async for GetProduct

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -25,7 +25,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
