68d98e15b95d09d298c1542a67b5458e15dc1beb
Eoin O'Malley <eomalley@letsgetchecked.com>
2022-05-11 11:30:14 +0100

    Set up error controller and add exception handling middleware

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,29 +3,26 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class ProductsController : BaseApiController
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    private readonly StoreContext _context;
+
+    public ProductsController(StoreContext context)
     {
-        private readonly StoreContext _context;
+        _context = context;
+    }
 
-        public ProductsController(StoreContext context)
-        {
-            _context = context;
-        }
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        return await _context.Products.ToListAsync();
+    }
 
-        [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
-        {
-            return await _context.Products.ToListAsync();
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
-        {
-            return await _context.Products.FindAsync(id);
-        }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        return await _context.Products.FindAsync(id);
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +25,8 @@ builder.Services.AddDbContext<StoreContext>(
 builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
