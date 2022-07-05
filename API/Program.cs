541773db653dccd8dc9813714d765fd76990549b
Eoin O'Malley <eomalley@letsgetchecked.com>
2022-07-05 17:53:46 +0100

    Create JWT authentication token service & configure Swagger to send the auth token

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,13 @@
+using System.Text;
 using API.Data;
 using API.Entities;
 using API.Middleware;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,22 +22,82 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+        c.AddSecurityDefinition(
+            "Bearer",
+            new OpenApiSecurityScheme
+            {
+                Description = "Jwt auth header",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            }
+        );
+        c.AddSecurityRequirement(
+            new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header
+                    },
+                    new List<string>()
+                }
+            }
+        );
+    }
+);
+
 builder.Services.AddDbContext<StoreContext>(
     opt =>
     {
         opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
     }
 );
+
 builder.Services.AddCors();
 
 // Identity
 builder.Services
-    .AddIdentityCore<User>()
+    .AddIdentityCore<User>(
+        opt =>
+        {
+            opt.User.RequireUniqueEmail = true;
+        }
+    )
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<StoreContext>();
-builder.Services.AddAuthentication();
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(
+        opt =>
+        {
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(configuration["JWTSettings:TokenKey"])
+                )
+            };
+        }
+    );
+
 builder.Services.AddAuthorization();
+builder.Services.AddScoped<TokenService>();
 
 var app = builder.Build();
 
@@ -57,6 +122,7 @@ app.UseCors(
     }
 );
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
