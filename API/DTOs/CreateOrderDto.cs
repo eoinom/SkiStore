4a642271cd7e2632fbcb5f8f4e8d0faa1146fc41
Eoin O'Malley <eomalley@letsgetchecked.com>
2022-10-10 18:50:02 +0100

    Created OrdersController

---
+++ API/DTOs/CreateOrderDto.cs
@@ -0,0 +1,9 @@
+using API.Entities.OrderAggregates;
+
+namespace API.DTOs;
+
+public class CreateOrderDto
+{
+    public bool SaveAddress { get; set; }
+    public ShippingAddress ShippingAddress { get; set; }
+}
--- API/Extensions/BasketExtensions.cs
+++ API/Extensions/BasketExtensions.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
 
@@ -27,5 +28,16 @@ public static class BasketExtensions
                 )
                 .ToList()
         };
+    }
+
+    public static IQueryable<Basket> RetrieveBasketWithItems(
+        this IQueryable<Basket> query,
+        string buyerId
+    )
+    {
+        return query
+            .Include(i => i.Items)
+            .ThenInclude(p => p.Product)
+            .Where(b => b.BuyerId == buyerId);
     }
 }
